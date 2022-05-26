eb6c6c060a2375414591919a0835566151e66ffc
killtothereal <krakus540@gmail.com>
2022-05-26 23:25:31 +0300

    new

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./source/bootstrap/css/bootstrap.css"/>
+    <link rel="stylesheet" href="./source/style.css"/>
+    <title>Document</title>
+</head>
+<body>
+    <div class='wrapper'>
+        <div style='display:flex; flex-direction:column; align-items:center; justify-content:center; width:30vw '>
+            <button type="button" class="btn btn-outline-dark" data-bs-toggle="button">Toggle button</button>
+            <button type="button" class="btn btn-outline-dark" data-bs-toggle="button">Toggle button2</button>
+            <button type="button" class="btn btn-outline-dark" data-bs-toggle="button">Toggle button3</button>
+        </div>
+    </div>
+</body>
+</html>
