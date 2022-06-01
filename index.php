abd44a9ff853850c444c4885af9f934ca58fb641
killtothereal <krakus540@gmail.com>
2022-06-01 20:28:38 +0300

    cut js

--- index.php
+++ index.php
@@ -111,6 +111,6 @@ if(!isset($_SESSION['admin']))
             <?php } ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-    <script type='text/javascript' src="./source/bootstrap/js/bootstrap.bundle.js"></script>
+    <script type='text/javascript' src="./source/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
